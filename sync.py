eb7250c08bfd900de4aa555b521a54d8ceb535b1
AOK21 <dev@aok21.com>
2021-02-01 01:49:24 +0200

    Updated sync service

--- sync.py
+++ sync.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from server.sync import sync_blocks
 
 background = BlockingScheduler()
-background.add_job(sync_blocks, "interval", seconds=1)
+background.add_job(sync_blocks, "interval", seconds=5)
 background.start()
